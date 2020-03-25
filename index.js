27899cb0f96590324631f8d1056eef77eb56f912
SaiKrishna Emmadishetty <S538340@nwmissouri.edu>
2020-03-24 22:10:41 -0500

    ADDED TEST FILES

---
+++ index.js
@@ -0,0 +1,26 @@
+const http = require('http')  // require built-in Node.js http package
+
+// Use hosting values if available, otherwise default 
+const hostname = process.env.hostname || '0.0.0.0' // allow remote access
+const port = process.env.PORT || 3002
+
+// define our server
+const server = http.createServer((req, res) => {
+  res.statusCode = 200
+  res.setHeader('Content-Type', 'text/plain')
+  
+
+  res.write('Welcome    \n')
+  res.write('About Node js \n')
+  res.write('Node.js is an open source,cross platform,JavaScript runtime environment that executes JavaScript code outside of a web browser.\n')
+  
+  
+  res.end('Thanks for visting.\n')
+})
+
+// start listening
+// use the server console to tell user where to find the server
+// use backticks for template literals with embedded expressions
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`)
+})
